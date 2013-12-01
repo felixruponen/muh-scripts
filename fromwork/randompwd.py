52d01ba74ae37d6278299a034f9014a5ba049b2e
daninjah <daninjah@gmail.com>
2013-12-01 19:17:07 +0100

    Initial commit

---
+++ fromwork/randompwd.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from random import choice
+from string import ascii_letters, digits, punctuation
+from sys import argv, exit
+
+def randompwd(num):
+	keylist = []
+	randompwd = ''
+	for i in ascii_letters + digits:
+		keylist.append(i)
+
+	for k in range(num):
+		randompwd += choice(keylist)
+	return randompwd
+
+def main():
+	if len(argv) == 2:
+		try:
+			num = int(argv[1])
+			print randompwd(num)
+		except:
+			print '\tuse integers, Luke!'
+			exit(1)
+	else:
+		print '\tusage: randompwd <length>'
+
+if __name__ == '__main__':
+	main()
