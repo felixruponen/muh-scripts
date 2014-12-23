7da8ee2bca9a00713c92e08af23a17ad88f0c6f1
Pablito <zelimkhan@mail.ru>
2014-12-23 12:40:10 +0100

    bash quickfix

--- routevpn.sh
+++ routevpn.sh
@@ -5,11 +5,11 @@
 # Iptables need updating as that rule doesn't feel secure at all
 
 ip addr show ppp0 &> /dev/null
-[ $? -eq 0 ] || { echo "no ppp0 device available"; exit 1 }
+[ $? -eq 0 ] || { echo "no ppp0 device available"; exit 1; }
 ip addr show eth1 &> /dev/null
-[ $? -eq 0 ] || { echo "no eth1 card available"; exit 1 }
+[ $? -eq 0 ] || { echo "no eth1 card available"; exit 1; }
 
-[ $UID -eq 0 ] || { echo "you're not a root"; exit 1 }
+[ $UID -eq 0 ] || { echo "you're not a root"; exit 1; }
 
 echo "Fixing the interface"
 ip addr add 192.168.117.1/24 dev eth1
